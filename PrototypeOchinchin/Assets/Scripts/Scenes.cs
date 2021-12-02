f98b412f367aeb0f71faf90e6b6cd38700c9cbac
MonoLLL <deniskiperman11@gmail.com>
2021-12-03 00:52:36 +0300

    MainMenu

---
+++ PrototypeOchinchin/Assets/Scripts/Scenes.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+    public void ChangeScenes(int numberScene)
+    {
+        SceneManager.LoadScene(numberScene);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
